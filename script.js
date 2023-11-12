3299fab73170a755db0f7c27045e6399098f6c22
denyschr <denprogman99@gmail.com>
2023-11-12 15:06:23 +0200

    update commit

--- script.js
+++ script.js
@@ -1,75 +1,32 @@
-const form = document.querySelector('#credit-card');
+// const form = document.querySelector('#credit-card');
+
+const cardInner = document.querySelector('.card__inner');
 
 const cardNumber = document.querySelector('#card-number');
 const cardHolder = document.querySelector('#card-holder');
-const cardExpiration = document.querySelector('#card-expiration');
-const cardCcv = document.querySelector('#card-ccv');
+const cardValidity = document.querySelector('#card-validity');
+const cardCvv = document.querySelector('#card-cvv');
+const cardEmail = document.querySelector('#card-email');
 
 const cardNumberText = document.querySelector('#card-number-text');
 const cardHolderText = document.querySelector('#card-holder-text');
-const cardExpirationText = document.querySelector('#card-expiration-text');
+const cardValidityText = document.querySelector('#card-validity-text');
+const cardCvvText = document.querySelector('#card-cvv-text');
 
-// const cardCcvText = document.querySelector('#card-ccv-text');
+const cardNumberLength = 19;
+const cardValidityLength = 5;
+const cardCvvLength = 3;
 
 cardNumber.addEventListener('input', e => {
-	const remark = e.target.nextElementSibling;
-	if (e.target.value.length < 19) {
-		remark.classList.add('_incomplete');
-		e.target.classList.add('_incomplete');
-		e.target.classList.remove('_complete')
-	} else {
-		remark.classList.remove('_incomplete');
-		e.target.classList.remove('_incomplete');
-		e.target.classList.add('_complete')
-	}
-});
+	const msg = e.target.nextElementSibling;
+	const isCorrectLength = e.target.value.length === cardNumberLength;
 
+	validateField(msg, e.target, isCorrectLength);
 
-cardHolder.addEventListener('input', e => {
-	const remark = e.target.nextElementSibling;
-	if (e.target.value.length < 1) {
-		remark.classList.add('_incomplete');
-		e.target.classList.add('_incomplete');
-		e.target.classList.remove('_complete')
-	} else {
-		remark.classList.remove('_incomplete');
-		e.target.classList.remove('_incomplete');
-		e.target.classList.add('_complete')
-	}
-});
-
-cardExpiration.addEventListener('input', e => {
-	const remark = e.target.nextElementSibling;
-	if (e.target.value.length <= 4) {
-		remark.classList.add('_incomplete');
-		e.target.classList.add('_incomplete');
-		e.target.classList.remove('_complete')
-	} else {
-		remark.classList.remove('_incomplete');
-		e.target.classList.remove('_incomplete');
-		e.target.classList.add('_complete')
-	}
-});
-
-cardCcv.addEventListener('input', e => {
-	const remark = e.target.nextElementSibling;
-	if (e.target.value.length < 3) {
-		remark.classList.add('_incomplete');
-		e.target.classList.add('_incomplete');
-		e.target.classList.remove('_complete')
-	} else {
-		remark.classList.remove('_incomplete');
-		e.target.classList.remove('_incomplete');
-		e.target.classList.add('_complete')
-	}
-});
-
-cardNumber.addEventListener('keyup', e => {
 	if (!e.target.value) {
-		cardNumberText.innerHTML = '**** **** **** ****';
+		cardNumberText.innerHTML = '●●●● ●●●● ●●●● ●●●●';
 	} else {
 		const valueOfInput = e.target.value.replaceAll(" ", "");
-
 		if (e.target.value.length > 14) {
 			e.target.value = valueOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
 			cardNumberText.innerHTML = valueOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
@@ -85,7 +42,12 @@ cardNumber.addEventListener('keyup', e => {
 	}
 });
 
-cardHolder.addEventListener('keyup', e => {
+cardHolder.addEventListener('input', e => {
+	const msg = e.target.nextElementSibling;
+	const isCorrectLength = e.target.value.length > 0;
+
+	validateField(msg, e.target, isCorrectLength);
+
 	if (!e.target.value) {
 		cardHolderText.innerHTML = 'FULL NAME';
 	} else {
@@ -93,22 +55,62 @@ cardHolder.addEventListener('keyup', e => {
 	}
 });
 
-cardExpiration.addEventListener('keyup', e => {
+cardValidity.addEventListener('input', e => {
+	const msg = e.target.nextElementSibling;
+	const isCorrectLength = e.target.value.length === cardValidityLength;
+
+	validateField(msg, e.target, isCorrectLength);
+
 	if (!e.target.value) {
-		cardExpirationText.innerHTML = 'MM/YY';
+		cardValidityText.innerHTML = 'MM/YY';
 	} else {
 		const valueOfInput = e.target.value.replace("/", "");
-
 		if (e.target.value.length > 2) {
 			e.target.value = valueOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
-			cardExpirationText.innerHTML = valueOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+			cardValidityText.innerHTML = valueOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
 		} else {
-			cardExpirationText.innerHTML = valueOfInput;
+			cardValidityText.innerHTML = valueOfInput;
 		}
 	}
 });
 
-cardCcv.addEventListener('keyup', e => {
-	let value = e.target.value;
-	e.target.value = value.replaceAll(/\w+/g, '*');
+cardCvv.addEventListener('input', e => {
+	const msg = e.target.nextElementSibling;
+	const isCorrectLength = e.target.value.length === cardCvvLength;
+
+	validateField(msg, e.target, isCorrectLength);
+
+	e.target.value = e.target.value.replaceAll(/\d/g, '*');
+	cardCvvText.innerHTML = e.target.value;
 });
+
+cardCvv.addEventListener('click', () => {
+	cardInner.classList.add('active');
+});
+
+document.addEventListener('click', e => {
+	if (e.target != cardCvv) {
+		cardInner.classList.remove('active');
+	}
+});
+
+cardEmail.addEventListener('input', e => {
+	const msg = e.target.nextElementSibling;
+	const isCorrectLength = e.target.value.slice(-10) === '@gmail.com';
+
+	validateField(msg, e.target, isCorrectLength);
+});
+
+function validateField(msg, currentField, length) {
+	msg.classList.toggle('has-error', !length);
+	currentField.classList.toggle('has-error', !length);
+	currentField.classList.toggle('is-correct', length);
+}
+
+function moveToNextField(currentField, nextFieldId) {
+	const maxLength = currentField.maxLength;
+
+	if (currentField.value.length === maxLength) {
+		document.getElementById(nextFieldId).focus();
+	}
+}
